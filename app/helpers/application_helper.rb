fc1140db2ad6c68161ad16d02b35375a77d97d9a
MyintThu <myintthu192019@gmail.com>
2024-06-25 14:40:20 +0630

    文字の装飾
    salckに並んでるようなやつかな、太字、斜体、リスト、引用、コードブロックとかを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+
+  def render_html(content)
+    sanitize(content, tags: %w(p span div br strong em a blockquote s ul ol li), attributes: %w(href class style))
+  end
 end
