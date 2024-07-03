28c424ad91ce223c02678f5e0426a03f40dafa02
MyintThu <myintthu192019@gmail.com>
2024-07-03 10:08:13 +0630

    fix design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,15 @@ module ApplicationHelper
   end
 
   def render_html(content)
-    sanitize(content, tags: %w(p span div br strong em a blockquote s ul ol li), attributes: %w(href class style))
+    return "" if content.nil?
+  
+    # Sanitize the content
+    content = sanitize(content, tags: %w(p span div br strong em a blockquote s ul ol li), attributes: %w(href class style))
+  
+    # Ensure all lines are wrapped in <p> tags
+   wrapped = content.split("\n").map { |line| "<p>#{line}</p>" }.join("\n")
+   wrapped.html_safe
+
   end
+
 end
