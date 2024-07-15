9acf8d4699f797fc019e44dedd1525f6cb398eff
MyintThu <myintthu192019@gmail.com>
2024-07-15 14:28:55 +0630

    emojiの追加、group messageにmulti mentionの追加
    group messageに一人以上mentionできるようにする

--- app/controllers/group_message_controller.rb
+++ app/controllers/group_message_controller.rb
@@ -12,15 +12,20 @@ class GroupMessageController < ApplicationController
     else
       message = params[:session][:message]
       draft_status = params[:session][:draft_status]
-      mention_name =params[:session][:memtion_name] 
+      mention_name = params[:session][:memtion_name]
+      if mention_name.present?
+        mention_names = JSON.parse(mention_name)
+      else
+        mention_names = []
+      end
       data = {
         "message": message,
         "s_channel_id": session[:s_channel_id],
-        "mention_name": [mention_name],
+        "mention_name": mention_names,
         "draft_message_status": draft_status,
-      };
+      }
       post_data("/groupmsg", data)
-     
+
       redirect_to m_channel_path(session[:s_channel_id])
     end
   end
@@ -37,14 +42,19 @@ class GroupMessageController < ApplicationController
     else
       message = params[:session][:message]
       draft_status = params[:session][:draft_status]
-      memtion_name = params[:session][:memtion_name] 
+      mention_name = params[:session][:memtion_name]
+      if mention_name.present?
+        mention_names = JSON.parse(mention_name)
+      else
+        mention_names = []
+      end
       data = {
         "s_group_message_id": session[:s_group_message_id],
         "s_channel_id": session[:s_channel_id],
         "message": message,
-        "mention_name": [memtion_name],
+        "mention_name": mention_names,
         "draft_message_status": draft_status,
-      };
+      }
       post_data("/groupthreadmsg", data)
       redirect_to t_group_message_path(session[:s_group_message_id])
     end
@@ -53,7 +63,7 @@ class GroupMessageController < ApplicationController
   def deletemsg
     if session[:s_channel_id].nil?
       redirect_to home_url
-    else  
+    else
       get_data("/delete_groupmsg?s_channel_id=#{session[:s_channel_id]}&id=#{params[:id]}&user_id=#{session[:current_user_id]}")
       redirect_to m_channel_path(session[:s_channel_id])
     end
@@ -68,7 +78,7 @@ class GroupMessageController < ApplicationController
     end
   end
 
-  # edit message 
+  # edit message
   def edit
     if params[:id].nil?
       redirect_to home_url
@@ -76,6 +86,7 @@ class GroupMessageController < ApplicationController
       response = get_data("/groupmsg/edit/#{params[:id]}")
       @dataraw = response["message"]["groupmsg"]
       @data = sanitize(@dataraw, tags: %w(p span div br strong em a blockquote s ul ol li), attributes: %w(href class style))
+      @mention_users = response["mentionusers"]
       retrieve_group_message
       retrievehome
       render "group_show/_edit"
@@ -83,14 +94,21 @@ class GroupMessageController < ApplicationController
   end
 
   def update
-    if params[:action_type] == 'send' || params[:session][:draft_status]
+    if params[:action_type] == "send" || params[:session][:draft_status]
       id = params[:id]
       message = params[:session][:message]
+      mention_name = params[:session][:memtion_name]
+      if mention_name.present?
+        mention_names = JSON.parse(mention_name)
+      else
+        mention_names = []
+      end
       data = {
         id: id,
-        message: message
+        message: message,
+        mention_name: mention_names,
       }
-      post_data( "update_groupmsg", data)
+      post_data("update_groupmsg", data)
       redirect_to m_channel_path(session[:s_channel_id])
     else
       redirect_to m_channel_path(session[:s_channel_id])
@@ -104,21 +122,29 @@ class GroupMessageController < ApplicationController
       response = get_data("/groupthreadmsg/edit/#{params[:id]}")
       @dataraw = response["message"]["groupthreadmsg"]
       @data = sanitize(@dataraw, tags: %w(p span div br strong em a blockquote s ul ol li), attributes: %w(href class style))
+      @mthread_users = response["mthreadusers"]
       retrieve_group_thread
       retrievehome
       render "group_thread_show/_editthread"
     end
   end
-  
+
   def update_thread
-    if params[:action_type] == 'send' || params[:session][:draft_status]
+    if params[:action_type] == "send" || params[:session][:draft_status]
       id = params[:id]
       message = params[:session][:message]
+      mention_name = params[:session][:memtion_name]
+      if mention_name.present?
+        mention_names = JSON.parse(mention_name)
+      else
+        mention_names = []
+      end
       data = {
         id: id,
-        message: message
+        message: message,
+        mention_name: mention_names,
       }
-      post_data( "update_groupthreadmsg", data)
+      post_data("update_groupthreadmsg", data)
       redirect_to t_group_message_path(session[:s_group_message_id])
     else
       redirect_to t_group_message_path(session[:s_group_message_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,94 +1,108 @@
 Rails.application.routes.draw do
+  root "static_pages#welcome"
+  get "welcome" => "static_pages#welcome"
 
-  root 'static_pages#welcome'
-  get 'welcome' => 'static_pages#welcome'
+  get "workspace" => "m_workspaces#new"
 
-  get'workspace' => 'm_workspaces#new'
+  get "signin" => "sessions#new"
+  post "signin" => "sessions#create"
 
-  get 'signin' =>  'sessions#new'
-  post 'signin' =>  'sessions#create'
+  get "change_password" => "change_password#new"
 
-  get 'change_password' => 'change_password#new'
+  get "home" => "static_pages#home"
 
-  get 'home' =>  'static_pages#home'
+  get "memberinvite" => "member_invitation#new"
+  post "memberinvite" => "member_invitation#invite"
+  get "confirminvitation" => "m_users#confirm"
 
-  get 'memberinvite' => 'member_invitation#new'
-  post 'memberinvite' => 'member_invitation#invite'
-  get 'confirminvitation' => 'm_users#confirm'
+  get "channelcreate" => "m_channels#new"
+  post "channelcreate" => "m_channels#create"
 
-  get 'channelcreate' => 'm_channels#new'
-  post 'channelcreate' => 'm_channels#create'
-  
-  get 'channeledit' => 'm_channels#edit'
-  get 'delete_channel' => 'm_channels#delete'
-  post 'channelupdate'=> 'm_channels#update'
+  get "channeledit" => "m_channels#edit"
+  get "delete_channel" => "m_channels#delete"
+  post "channelupdate" => "m_channels#update"
 
-  get 'star' => 't_direct_star_msg#create'
-  get 'unstar' => 't_direct_star_msg#destroy'
-  get 'starthread' => 't_direct_star_thread#create'
-  get 'unstarthread' => 't_direct_star_thread#destroy'
+  get "star" => "t_direct_star_msg#create"
+  get "unstar" => "t_direct_star_msg#destroy"
+  get "starthread" => "t_direct_star_thread#create"
+  get "unstarthread" => "t_direct_star_thread#destroy"
 
-  get 'delete_directmsg' => "direct_message#deletemsg"
-  get 'delete_directthread' => "direct_message#deletethread"
+  get "delete_directmsg" => "direct_message#deletemsg"
+  get "delete_directthread" => "direct_message#deletethread"
 
-  get 'delete_groupmsg' => "group_message#deletemsg"
-  get 'delete_groupthread' => "group_message#deletethread"
+  get "delete_groupmsg" => "group_message#deletemsg"
+  get "delete_groupthread" => "group_message#deletethread"
 
-  get 'groupstar' => 't_group_star_msg#create'
-  get 'groupunstar' => 't_group_star_msg#destroy'
-  get 'groupstarthread' => 't_group_star_thread#create'
-  get 'groupunstarthread' => 't_group_star_thread#destroy'
+  get "groupstar" => "t_group_star_msg#create"
+  get "groupunstar" => "t_group_star_msg#destroy"
+  get "groupstarthread" => "t_group_star_thread#create"
+  get "groupunstarthread" => "t_group_star_thread#destroy"
 
-  get 'starlists' => 'star_lists#show'
-  get 'thread' => 'thread#show'
-  get 'mentionlists' => 'mention_lists#show'
-  get 'allunread' => 'all_unread#show'
-  get 'draftlists' => 'draft_lists#show'
+  get "starlists" => "star_lists#show"
+  get "thread" => "thread#show"
+  get "mentionlists" => "mention_lists#show"
+  get "allunread" => "all_unread#show"
+  get "draftlists" => "draft_lists#show"
 
-  get 'usermanage' => 'user_manage#usermanage'
-  get 'edit' => 'user_manage#edit'
-  get 'update' => 'user_manage#update'
+  get "usermanage" => "user_manage#usermanage"
+  get "edit" => "user_manage#edit"
+  get "update" => "user_manage#update"
 
-  get 'channeluser' => 'channel_user#show'
-  get 'channeluseradd' => 'channel_user#create'
-  get 'channeluserdestroy' => 'channel_user#destroy'
-  get 'channeluserleave' => 'channel_user#leave'
-  get 'channeluserjoin' => 'channel_user#join'
+  get "channeluser" => "channel_user#show"
+  get "channeluseradd" => "channel_user#create"
+  get "channeluserdestroy" => "channel_user#destroy"
+  get "channeluserleave" => "channel_user#leave"
+  get "channeluserjoin" => "channel_user#join"
 
-  post 'directmsg' => 'direct_message#show'
-  post 'directthreadmsg' => 'direct_message#showthread'
-  
-  post 'groupmsg' => 'group_message#show'
-  post 'groupthreadmsg' => 'group_message#showthread'
+  post "directmsg" => "direct_message#show"
+  post "directthreadmsg" => "direct_message#showthread"
 
-  get 'refresh' => 'sessions#refresh'
+  post "groupmsg" => "group_message#show"
+  post "groupthreadmsg" => "group_message#showthread"
 
-  get 'refresh_direct' => 'm_users#refresh_direct'
-  get 'refresh_group' => 'm_channels#refresh_group'
+  get "refresh" => "sessions#refresh"
 
-  delete 'logout' =>  'sessions#destroy'
+  get "refresh_direct" => "m_users#refresh_direct"
+  get "refresh_group" => "m_channels#refresh_group"
+
+  delete "logout" => "sessions#destroy"
 
   # edit username
-  get 'useredit' => 'm_users#edituser'
-  patch 'useredit' => 'm_users#updateuser'
+  get "useredit" => "m_users#edituser"
+  patch "useredit" => "m_users#updateuser"
 
-  # edit direct message 
-  get '/directmsg/edit/:id' => 'direct_message#edit', as: 'edit_directmsg'
-  post 'update_directmsg' => 'direct_message#update'
+  # edit direct message
+  get "/directmsg/edit/:id" => "direct_message#edit", as: "edit_directmsg"
+  post "update_directmsg" => "direct_message#update"
   # edit direct thread
-  get '/directthreadmsg/edit/:id' => 'direct_message#edit_thread', as: 'edit_directthreadmsg'
-  post 'update_directthreadmsg' => 'direct_message#update_thread'
+  get "/directthreadmsg/edit/:id" => "direct_message#edit_thread", as: "edit_directthreadmsg"
+  post "update_directthreadmsg" => "direct_message#update_thread"
   # edit group message
-  get '/groupmsg/edit/:id' => 'group_message#edit', as: 'edit_groupmsg'
-  post 'update_groupmsg'  => 'group_message#update'
+  get "/groupmsg/edit/:id" => "group_message#edit", as: "edit_groupmsg"
+  post "update_groupmsg" => "group_message#update"
   # edit group thread
-  get '/groupthreadmsg/edit/:id' => 'group_message#edit_thread', as: 'edit_groupthreadmsg'
-  post 'update_groupthreadmsg' => 'group_message#update_thread'
+  get "/groupthreadmsg/edit/:id" => "group_message#edit_thread", as: "edit_groupthreadmsg"
+  post "update_groupthreadmsg" => "group_message#update_thread"
+
+  # start routes for direct react
+  post "directreact" => "t_direct_react_msg#create"
+  get "directreact" => "t_direct_react_msg#create"
+
+  post "directthreadreact" => "t_direct_react_thread#create"
+  get "directthreadreact" => "t_direct_react_thread#create"
+  # end routes for direct react
+
+  # start routes for group react
+  post "groupreact" => "t_group_react_msg#create"
+  get "groupreact" => "t_group_react_msg#create"
+
+  post "groupthreadreact" => "t_group_react_thread#create"
+  get "groupthreadreact" => "t_group_react_thread#create"
+  # end routes for group react
 
   resources :m_workspaces
   resources :m_users
   resources :m_channels
   resources :t_direct_messages
   resources :t_group_messages
-
 end
